7983b4dc17c6c49ff04895c9b15e1ce746ce5b6c
emanuelbalcazar <emanuel.balcazar1@claro.com.ar>
2021-04-16 18:34:40 -0300

    Se agrega el codigo base del desafio de la clase 2 con (**)

---
+++ ejemplos/02-ecmascript/01-ejercicio-potencia.js
@@ -0,0 +1,11 @@
+/**
+ * Realizar una funcion que devuelva un array y devuelva el resultado de
+ * la operacion de potenciacion en caso de poder realizarla.
+ * De no ser posible, devolver null.
+ * Consejo: se puede usar eval(exp) y array.includes
+ */
+var array = ['6**2', '**', '3**3', '4**', '4**5', '8**2**', '4*=5'];
+
+array.forEach(exp => {
+    // completar el codigo
+});
