911206901a4fd71046bd47e0bbb1153cccdceabf
emanuelbalcazar <emanuel.balcazar1@claro.com.ar>
2021-04-20 08:57:45 -0300

    Se agrega typescript y un ejemplo basico de codigo tipado

---
+++ ejemplos/02-ecmascript/17-intro-typescript.ts
@@ -0,0 +1,18 @@
+// boolean
+let done : boolean = false;
+
+// number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// string
+let color: string = "blue";
+color = "red";
+
+// array
+let lista: number[] = [1, 2, 3];
+let lista2: Array<number> = [4, 5, 6];
+
+console.log(lista);
