89df0d491a7159e8b399bf434a28a0dbe97741af
emanuelbalcazar <emanuel.balcazar1@claro.com.ar>
2021-04-20 16:14:07 -0300

    Se agrega el codigo base para el desafio de la clase 3

---
+++ ejemplos/03-programacion-asincrona/07-desafio-asincrono.js
@@ -0,0 +1,18 @@
+/**
+ * desarrollar la funcion mostrarLetras que reciba un string como
+ * parametro y permita mostrar una vez por segundo cada uno de sus
+ * caracteres.
+ * Tiempo aproximado: 20-25 minutos
+ */
+
+const sleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+const mostrarLetras = async (cadena, delay, done) => {
+    // completar el codigo
+}
+
+// llamar a las funciones con 0, 250 y 500 ms
