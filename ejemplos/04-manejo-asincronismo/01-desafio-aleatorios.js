3c8ed87af6035a3c2b5a6046232a442cc5310e02
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2021-05-06 15:02:49 -0300

    Correcciones menores de ejemplo

--- ejemplos/04-manejo-asincronismo/01-desafio-aleatorios.js
+++ ejemplos/04-manejo-asincronismo/01-desafio-aleatorios.js
@@ -2,11 +2,20 @@
  * Realizar una funcion que devuelva numeros aleatorios entre un minimo y maximo
  * configurable y que muestre la cantidad de valores que vamos solicitando junto a la
  * fecha y hora del pedido.
+ * {orden: 1, numero: 5, fyh: dd/mm/yy hh:mm:ss}
  * AVISO: No se permite usar variables globales ni contadores para mantener el contador.
  */
 function obtenerRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function* hacerID() {
+
+}
+
+let obtenerId = hacerID();
+
+
 
 // completar el codigo...
 
