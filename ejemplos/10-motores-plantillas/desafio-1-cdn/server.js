8157511dfdb494ee7cbc5c3b8abb243c1787c701
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2021-06-03 20:04:11 -0300

    Se actualiza la plantilla para el desafio 1

--- ejemplos/10-motores-plantillas/desafio-1-cdn/server.js
+++ ejemplos/10-motores-plantillas/desafio-1-cdn/server.js
@@ -1,14 +1,13 @@
-import express from 'express'
+const express = require('express');
+const app = express();
 
-const app = express()
-
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 
 /* ------------------------------------------------------ */
 /* Server Listen */
 const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${server.address().port}`)
-})
+});
+
 server.on('error', error => console.log(`Error en servidor ${error}`))
