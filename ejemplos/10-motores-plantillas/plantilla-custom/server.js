c6f4434750cfd0ba54d6b3d5c4b3971bde56522c
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2021-06-03 21:47:24 -0300

    Ejemplo de motor de plantilla coder con estilos css

--- ejemplos/10-motores-plantillas/plantilla-custom/server.js
+++ ejemplos/10-motores-plantillas/plantilla-custom/server.js
@@ -7,24 +7,28 @@ const express = require('express');
 const app = express();
 const puerto = 8080;
 
+app.use(express.static('public'));
+
 // defino el motor de plantilla
-app.engine('ntl', (filepath, options, callback) => {
+app.engine('coder', (filepath, options, callback) => {
     fs.readFile(filepath, (err, content) => {
         if (err)
             return callback(err);
 
+        console.log(content.toString())
+
         let rendered = content.toString().replace('#title#', '' + options.title + '')
-            .replace('#message#', '' + options.message + '');
+            .replace('#message#', '' + options.message + '').replace('#day#', options.day);
 
         return callback(null, rendered);
     });
 });
 
 app.set('views', './views'); // especifica el directorio de las vistas
-app.set('view engine', 'ntl'); // registra el motor de plantilla
+app.set('view engine', 'coder'); // registra el motor de plantilla
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hola', message: 'Mundo!' });
+    res.render('index', { title: 'Hola', message: 'Mundo!', day: "Jueves" });
 });
 
 // pongo a escuchar el servidor en el puerto indicado
