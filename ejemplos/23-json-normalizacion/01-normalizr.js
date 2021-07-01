3582fa864ab9bb3578647deefa0ce0714b22c019
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2021-07-01 18:26:40 -0300

    Se agrega el archivo para normalizar de la clase 23

--- ejemplos/23-json-normalizacion/01-normalizr.js
+++ ejemplos/23-json-normalizacion/01-normalizr.js
@@ -23,20 +23,20 @@ const blogPost = {
     ]
 }
 
-// Define a users schema
+// defino el esquema de los usuarios (o comentadores)
 const user = new schema.Entity('users');
 
-// Define your comments schema
+// defino el esquema de los comentarios, que son realizados por un usuario
 const comment = new schema.Entity('comments', {
     commenter: user
 });
 
-// Define your article
+// defino el esquema de los articulos, que tienen un autor y una cantidad de comentarios
 const article = new schema.Entity('articles', {
     author: user,
     comments: [comment]
 });
 
+// creamos el objeto normalizado
 const normalizedData = normalize(blogPost, article);
-
 console.log(JSON.stringify(normalizedData, null, 3));
---
+++ ejemplos/23-json-normalizacion/desafio1/empresa.js
@@ -0,0 +1,294 @@
+module.exports.empresa = {
+    id: "1000",
+    nombre: "Coderhouse",
+    gerente: {
+        id: "2",
+        nombre: "Pedro",
+        apellido: "Mei",
+        DNI: "20442639",
+        direccion: "CABA 457",
+        telefono: "1567811544"
+    },
+    encargado: {
+        id: "3",
+        nombre: "Pablo",
+        apellido: "Blanco",
+        DNI: "20442640",
+        direccion: "CABA 458",
+        telefono: "1567811545"
+    },
+    empleados: [
+        {
+            id: "1",
+            nombre: "Nicole",
+            apellido: "Gonzalez",
+            DNI: "20442638",
+            direccion: "CABA 456",
+            telefono: "1567811543"
+        },
+        {
+            id: "2",
+            nombre: "Pedro",
+            apellido: "Mei",
+            DNI: "20442639",
+            direccion: "CABA 457",
+            telefono: "1567811544"
+        },
+        {
+            id: "3",
+            nombre: "Pablo",
+            apellido: "Blanco",
+            DNI: "20442640",
+            direccion: "CABA 458",
+            telefono: "1567811545"
+        },
+        {
+            id: "4",
+            nombre: "Ana",
+            apellido: "Rojo",
+            DNI: "20442641",
+            direccion: "CABA 459",
+            telefono: "1567811546"
+        },
+        {
+            id: "5",
+            nombre: "Lucia",
+            apellido: "Sorbo",
+            DNI: "20442642",
+            direccion: "CABA 460",
+            telefono: "1567811547"
+        },
+        {
+            id: "6",
+            nombre: "Jose",
+            apellido: "Pieres",
+            DNI: "20442643",
+            direccion: "CABA 461",
+            telefono: "1567811548"
+        },
+        {
+            id: "7",
+            nombre: "Maria",
+            apellido: "Lopez",
+            DNI: "20442644",
+            direccion: "CABA 462",
+            telefono: "1567811549"
+        }
+    ]
+}
+
+module.exports.hold = {
+    id: "10000",
+    empresas: [
+        {
+            id: "1000",
+            nombre: "Coderhouse",
+            gerente: {
+                id: "2",
+                nombre: "Pedro",
+                apellido: "Mei",
+                DNI: "20442639",
+                direccion: "CABA 457",
+                telefono: "1567811544"
+            },
+            encargado: {
+                id: "3",
+                nombre: "Pablo",
+                apellido: "Blanco",
+                DNI: "20442640",
+                direccion: "CABA 458",
+                telefono: "1567811545"
+            },
+            empleados: [
+                {
+                    id: "1",
+                    nombre: "Nicole",
+                    apellido: "Gonzalez",
+                    DNI: "20442638",
+                    direccion: "CABA 456",
+                    telefono: "1567811543"
+                },
+                {
+                    id: "2",
+                    nombre: "Pedro",
+                    apellido: "Mei",
+                    DNI: "20442639",
+                    direccion: "CABA 457",
+                    telefono: "1567811544"
+                },
+                {
+                    id: "3",
+                    nombre: "Pablo",
+                    apellido: "Blanco",
+                    DNI: "20442640",
+                    direccion: "CABA 458",
+                    telefono: "1567811545"
+                },
+                {
+                    id: "4",
+                    nombre: "Ana",
+                    apellido: "Rojo",
+                    DNI: "20442641",
+                    direccion: "CABA 459",
+                    telefono: "1567811546"
+                },
+                {
+                    id: "5",
+                    nombre: "Lucia",
+                    apellido: "Sorbo",
+                    DNI: "20442642",
+                    direccion: "CABA 460",
+                    telefono: "1567811547"
+                },
+                {
+                    id: "6",
+                    nombre: "Jose",
+                    apellido: "Pieres",
+                    DNI: "20442643",
+                    direccion: "CABA 461",
+                    telefono: "1567811548"
+                },
+                {
+                    id: "7",
+                    nombre: "Maria",
+                    apellido: "Lopez",
+                    DNI: "20442644",
+                    direccion: "CABA 462",
+                    telefono: "1567811549"
+                }
+            ]
+        },
+        {
+            id: "1001",
+            nombre: "Coderhouse2",
+            gerente: {
+                id: "6",
+                nombre: "Jose",
+                apellido: "Pieres",
+                DNI: "20442643",
+                direccion: "CABA 461",
+                telefono: "1567811548"
+            },
+            encargado: {
+                id: "5",
+                nombre: "Lucia",
+                apellido: "Sorbo",
+                DNI: "20442642",
+                direccion: "CABA 460",
+                telefono: "1567811547"
+            },
+            empleados: [
+                {
+                    id: "1",
+                    nombre: "Nicole",
+                    apellido: "Gonzalez",
+                    DNI: "20442638",
+                    direccion: "CABA 456",
+                    telefono: "1567811543"
+                },
+                {
+                    id: "2",
+                    nombre: "Pedro",
+                    apellido: "Mei",
+                    DNI: "20442639",
+                    direccion: "CABA 457",
+                    telefono: "1567811544"
+                },
+                {
+                    id: "5",
+                    nombre: "Lucia",
+                    apellido: "Sorbo",
+                    DNI: "20442642",
+                    direccion: "CABA 460",
+                    telefono: "1567811547"
+                },
+                {
+                    id: "6",
+                    nombre: "Jose",
+                    apellido: "Pieres",
+                    DNI: "20442643",
+                    direccion: "CABA 461",
+                    telefono: "1567811548"
+                },
+                {
+                    id: "7",
+                    nombre: "Maria",
+                    apellido: "Lopez",
+                    DNI: "20442644",
+                    direccion: "CABA 462",
+                    telefono: "1567811549"
+                },
+                {
+                    id: "8",
+                    nombre: "Lucio",
+                    apellido: "Garcia",
+                    DNI: "20442645",
+                    direccion: "CABA 463",
+                    telefono: "1567811550"
+                }
+            ]
+        },
+        {
+            id: "1002",
+            nombre: "Coderhouse3",
+            gerente: {
+                id: "9",
+                nombre: "Diego",
+                apellido: "Sojo",
+                DNI: "20442646",
+                direccion: "CABA 464",
+                telefono: "1567811551"
+            },
+            encargado: {
+                id: "8",
+                nombre: "Lucio",
+                apellido: "Garcia",
+                DNI: "20442645",
+                direccion: "CABA 463",
+                telefono: "1567811550"
+            },
+            empleados: [
+                {
+                    id: "4",
+                    nombre: "Ana",
+                    apellido: "Rojo",
+                    DNI: "20442641",
+                    direccion: "CABA 459",
+                    telefono: "1567811546"
+                },
+                {
+                    id: "5",
+                    nombre: "Lucia",
+                    apellido: "Sorbo",
+                    DNI: "20442642",
+                    direccion: "CABA 460",
+                    telefono: "1567811547"
+                },
+                {
+                    id: "6",
+                    nombre: "Jose",
+                    apellido: "Pieres",
+                    DNI: "20442643",
+                    direccion: "CABA 461",
+                    telefono: "1567811548"
+                },
+                {
+                    id: "7",
+                    nombre: "Maria",
+                    apellido: "Lopez",
+                    DNI: "20442644",
+                    direccion: "CABA 462",
+                    telefono: "1567811549"
+                },
+                {
+                    id: "9",
+                    nombre: "Diego",
+                    apellido: "Sojo",
+                    DNI: "20442646",
+                    direccion: "CABA 464",
+                    telefono: "1567811551"
+                }
+            ]
+        }
+    ]
+}
