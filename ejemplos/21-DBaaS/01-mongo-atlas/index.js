77afbb0d6b1d3d840fb34a02203b722f8b36125a
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2021-07-13 18:58:29 -0300

    Se agrega el ejemplo de conexion a mongo atlas

--- ejemplos/21-DBaaS/01-mongo-atlas/index.js
+++ ejemplos/21-DBaaS/01-mongo-atlas/index.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://root:root@cluster0.1w8lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(uri,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, error => {
-        if (error)
-            console.log(error);
-        else
-            console.log('Conectado a mongo atlas');
-    });
+const MyModel = mongoose.model('users', new mongoose.Schema({ nombre: String, apellido: String }));
+
+async function connect() {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(`mongoose conectado en ${uri}`);
+    await MyModel.create({ nombre: 'Emanuel', apellido: 'Balcazar' });
+    return;
+}
+
+connect();
